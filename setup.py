b9110a0439f380a95ff697688f74d1a881cd5531
Bensonaa <aa.benson1@gmail.com>
2021-03-19 12:03:10 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ def setup_package():
             "tornado",
             "twisted",
             "urllib3",
+            "spacy"
         ],
         entry_points={
             "console_scripts": [
